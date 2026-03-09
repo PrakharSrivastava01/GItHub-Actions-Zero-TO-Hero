070f278c9ff3248adfb901a2ef75471240f55816
Chintamani Mohanta <chintamanimohant2@gmail.com>
2026-03-09 21:52:59 +0530

    added app.py

--- templets/app.py
+++ templets/app.py
@@ -1,6 +1,12 @@
-services:
+from flask import Flask, render_template
+app = Flask(__name__)
 
-  web:
-    image: ${DOCKERHUB_USER}/github-actions-app:${DOCKER_TAG}
-    ports:
-     - "80:80"
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/health')
+def health():
+    return 'Server is up and running'
